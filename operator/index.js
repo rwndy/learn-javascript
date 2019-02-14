6aafff956d5cd9b74fdbea59a57f5b5d3821266b
Riwandy <riwandy10@gmail.com>
2019-02-15 00:37:14 +0700

    update code

---
+++ operator/index.js
@@ -0,0 +1,23 @@
+// ********************************************//
+// Operator pada JavaScript sama seperti operator pada matematika
+// contoh
+
+let tambah = 2+2;
+let pengurangan = 10-2;
+let perkalian = 20*10;
+let pembagian = 20/10;
+let sisaBagi = 7%2;
+
+console.log(`penjumlahan: ${tambah}`);
+console.log(`pengurangan: ${pengurangan}`);
+console.log(`perkalian: ${perkalian}`);
+console.log(`pembagian: ${pembagian}`);
+console.log(`sisa bagi: ${sisaBagi}`);
+
+let tahunSekarang = 2019;
+let tahunLahir = 1990;
+
+let umur = tahunSekarang - tahunLahir;
+console.log(`Umur Aceng: ${umur}`);
+
+
