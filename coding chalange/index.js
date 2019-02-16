bf36f9c3cf584ab62a6679c1d15980283721db17
Riwandy <riwandy10@gmail.com>
2019-02-16 07:54:44 +0700

    update code

---
+++ coding chalange/index.js
@@ -0,0 +1,26 @@
+let massAceng = 70;
+let massFikri = 65;
+
+let heightAceng = 1.78;
+let heightFikri = 1.75;
+
+let bmiAceng = massAceng / (heightAceng**2);
+let bmiFikri = massFikri / (heightFikri**2);
+
+let heigher = heightAceng > heightFikri;
+
+// console
+console.log(massAceng);
+console.log(massFikri);
+console.log(heightAceng);
+console.log(heightFikri);
+console.log(bmiAceng);
+console.log(bmiFikri);
+
+console.log(`Is Aceng's BMI higher than Fikri's true? ${heigher}`);
+
+
+
+
+
+
