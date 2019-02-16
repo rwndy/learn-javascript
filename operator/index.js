5c60422e499f23bb00c926fe15e6f842395d7697
Riwandy <riwandy10@gmail.com>
2019-02-16 07:44:51 +0700

    add coding operator

--- operator/index.js
+++ operator/index.js
@@ -7,12 +7,14 @@ let pengurangan = 10-2;
 let perkalian = 20*10;
 let pembagian = 20/10;
 let sisaBagi = 7%2;
+let pangkat = 7**2;
 
 console.log(`penjumlahan: ${tambah}`);
 console.log(`pengurangan: ${pengurangan}`);
 console.log(`perkalian: ${perkalian}`);
 console.log(`pembagian: ${pembagian}`);
 console.log(`sisa bagi: ${sisaBagi}`);
+console.log(`perpangkatan: ${pangkat}`);
 
 let tahunSekarang = 2019;
 let tahunLahir = 1990;
@@ -21,3 +23,7 @@ let umur = tahunSekarang - tahunLahir;
 console.log(`Umur Aceng: ${umur}`);
 
 
+let logical = tambah < pengurangan;
+let logical1 = tambah > pengurangan;
+console.log(`hasilnya adalah: ${logical}`);
+console.log(`hasilnya adalah: ${logical1}`);
