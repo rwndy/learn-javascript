e3751adf973744a0da01809ddcf705bb596ef626
Riwandy <riwandy10@gmail.com>
2019-02-16 10:27:43 +0700

    conditional code

---
+++ conditional/index.js
@@ -0,0 +1,25 @@
+// if else
+
+let perut = 'laper';
+
+if (perut === 'laper') {
+  console.log('makan bang');
+  
+} else {
+  console.log('puasa 10 tahun'); 
+}
+
+let umurAceng = 29;
+let umurFikri = 28;
+
+if (umurAceng <= umurFikri) {
+  console.log('Tua banget lu Fik');
+  
+} else if (umurAceng >= umurFikri) {
+  console.log('tua juga lu ceng');
+  
+} else {
+  console.log('hehehe');
+  
+}
+
