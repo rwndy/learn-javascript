4aa48f2954f0d4a42ee97791e02dfebf635838a8
Riwandy <riwandy10@gmail.com>
2019-02-23 15:05:52 +0700

    update code

--- function/index.js
+++ function/index.js
@@ -1,3 +1,5 @@
+// ini adalah function deklarasi
+
 function Pagi() {
   return console.log('selamat pagi sayang');
 }
@@ -23,3 +25,41 @@ function curhatLagi() {
 }
 
 curhatLagi();
+
+// menampung nilai menggunakan return
+// return hanya bisa menampung satu nilai saja.
+
+function pagi() {
+  return 'selamat pagai, kamu apa kabar?'
+}
+
+let salam = pagi;
+console.log(salam);
+
+
+function makan() {
+  let buah = ['melon', 'semangka'];
+  return buah;
+}
+
+console.log(makan());
+
+function umurKamu(TahunLahir) {
+  return 2019 - TahunLahir;
+}
+
+let umurAceng = umurKamu(1996);
+let umurFikri = umurKamu(1994);
+
+console.log(`ini adalah umur Aceng: ${umurAceng} dan umur Fikri: ${umurFikri}`);
+
+function Sore() {
+  let AllSore = ['afternoon', 'petang', 'peuting'];
+  return AllSore;
+}
+
+let Salam = Sore();
+for (let value of Salam) {
+  console.log(value);
+  
+}
