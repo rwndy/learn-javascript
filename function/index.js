51949585a6631a95f569d873fef63f08ccb95291
Riwandy <riwandy10@gmail.com>
2019-02-23 11:39:35 +0700

    create code function

---
+++ function/index.js
@@ -0,0 +1,25 @@
+function Pagi() {
+  return console.log('selamat pagi sayang');
+}
+
+Pagi();
+
+function kabar() {
+  console.log('kamu, apa kabar?');
+  
+}
+
+kabar();
+
+function curhat() {
+  console.log('aku sehat dan baik-baik juga', 'kamu liat badan aku kurusan banget ya?');
+}
+
+curhat();
+
+function curhatLagi() {
+  console.log('aku juga gak tau, kenapa badan aku bisa habis kayak gini.', ' tapi, aku seneng liat kamu lagi di RC tahun ini');
+  
+}
+
+curhatLagi();
