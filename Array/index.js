097dc7c48ee307afd6419343b0e0e9f1843c1299
Riwandy <riwandy10@gmail.com>
2019-02-25 03:21:44 +0700

    add code

---
+++ Array/index.js
@@ -0,0 +1,9 @@
+let fruits = ['orange', 'grape', 'banana', 'melon'];
+console.log(fruits); //membuat array yang diisi nilainya dengan string
+
+let urutan = [1, 2, 3, 4, 5, 6];
+console.log(urutan); // membuat array yang diisi nilainya dengan number
+
+//menampilkan panjang dari isi array
+console.log(fruits.length);
+console.log(urutan.length);
