072154006a848a9745c2faab023e7a6e12e20bfb
Riwandy <riwandy10@gmail.com>
2019-02-25 16:36:14 +0700

    update code array

--- Array/index.js
+++ Array/index.js
@@ -7,3 +7,6 @@ console.log(urutan); // membuat array yang diisi nilainya dengan number
 //menampilkan panjang dari isi array
 console.log(fruits.length);
 console.log(urutan.length);
+
+let mix = [false, 'string', 22, ['array lagi', 33, true]]; //membuat array campuran dan didalam array ada array lagi
+console.log(mix);
