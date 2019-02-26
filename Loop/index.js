bee3179461a5a7046f053019a45338e831ea7ef3
Riwandy <riwandy10@gmail.com>
2019-02-27 00:16:32 +0700

    add code loop

---
+++ Loop/index.js
@@ -0,0 +1,16 @@
+let i = 1;
+for (i; i <= 10; i++) {
+  console.log(`nomor urut: ${i}`);
+}
+
+// perulangan dengan nilai ganjil
+let j = 1
+for (j; j <= 20; j= j+2) {
+  console.log(`nomor urut: ${j}`);
+}
+
+// perulangan dengan nilai genap
+let k = 0
+for (k; k <= 20; k = k+2) {
+  console.log(`nomor urut: ${k}`);
+}
