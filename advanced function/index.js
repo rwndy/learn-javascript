a9606cb4849d32dd1cc79daf1eca72be3b0f295e
Riwandy <riwandy10@gmail.com>
2019-03-13 15:27:35 +0700

    add code advanced function

--- advanced function/index.js
+++ advanced function/index.js
@@ -16,3 +16,22 @@ function inputan(first_name, last_name, CallBack) {
 }
 
 inputan('hartika', 'yusman', callBack);
+
+// contoh lain callback function
+
+let years = [1990, 1996, 2000, 2004];
+
+function calc(arr, fn) {
+  let arrResult = []; // variabel ini akan diisi nantinya untuk menghitung panjang array
+  for (let i = 0; i < arr.length; i++) {
+    arrResult.push(fn(arr[i]));
+  }
+  return arrResult;
+}
+
+function calculacAge(e) {
+  return 2019 - e;
+}
+
+let ages = calc(years, calculacAge);
+console.log(`hasil umur yang dikalkulasikan ${ages}`);
